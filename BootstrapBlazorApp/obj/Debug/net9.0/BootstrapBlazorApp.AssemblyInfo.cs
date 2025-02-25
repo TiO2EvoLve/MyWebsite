b6047eb1df0ac8de2676d1839b8f4e16209c8099
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2025-02-25 11:46:10 +0800

    index.html

--- BootstrapBlazorApp/obj/Debug/net9.0/BootstrapBlazorApp.AssemblyInfo.cs
+++ BootstrapBlazorApp/obj/Debug/net9.0/BootstrapBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BootstrapBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc181b16a50f570b74fd5ef1023ef9d5b67d655c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BootstrapBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BootstrapBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
